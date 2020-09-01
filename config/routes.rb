79d4d66cd453e5bc83fcf7983c1a52d22174c9e7
arizar98 <arifzahiruddin@gmail.com>
2020-09-01 16:07:24 +0800

    Chapter 12

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   scope "(:locale)", locale: /en/ do
     get "users/show"
@@ -12,6 +14,7 @@ Rails.application.routes.draw do
 
     resources :users
     resources :account_activations, only: :edit
+    resources :password_resets, only: [:new, :create, :edit, :update]
 
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
