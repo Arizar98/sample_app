3bd8d9cd26cbb2bed7c3f45eb6f0332d27d6f634
arizar98 <arifzahiruddin@gmail.com>
2020-09-05 08:10:21 +0800

    Chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "welcome_to_the_sample_app!"
       redirect_to @user
     else
@@ -22,7 +23,6 @@ class UsersController < ApplicationController
   end
 
   private
-
 
   def user_params
     params.require(:user).permit :name, :email, :password, :password_confirmation
